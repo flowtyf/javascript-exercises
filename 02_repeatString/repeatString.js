0cac09776b63546f837d02786f443b9f0fcf82c1
flowtyf <taiao.kawiti@developersinstitute.co.nz>
2023-03-16 15:00:50 +1300

    remove unecssary NaN check

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,7 @@
 const repeatString = function (string, num) {
   let returnString = '';
   if (num >= 0) {
-    for (let i = 0; i < num && string !== NaN; i++) {
+    for (let i = 0; i < num; i++) {
       returnString += string;
     }
     return returnString;
