1f8585fe599f752e47da3ed73ef9b85903ecf8da
flowtyf <taiao.kawiti.wbhs@gmail.com>
2023-03-18 20:11:43 +1300

    Add my solution for 05 sumall

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
 const removeFromArray = function (array, ...args) {
   let newArr = [];
   array.forEach((item) => {
+    // the not logical operator means that "is not a part of args" - invalidates the value of something from true to false
     if (!args.includes(item)) {
       newArr.push(item);
     }
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
+  let greaterVal;
+  let lesserVal;
 
+  if (a > 0 && b > 0 && typeof a !== "string" && typeof b !== "string") {
+    switch (a > b) {
+      case true:
+        greaterVal = a;
+        lesserVal = b;
+        break;
+      case false:
+        greaterVal = b;
+        lesserVal = a;
+        break;
+      default:
+        return "ERROR";
+    }
+
+    for (let i = lesserVal; i <= greaterVal; i++) {
+      sum += lesserVal;
+      lesserVal++;
+    }
+  } else return "ERROR";
+
+  return sum;
 };
 
 // Do not edit below this line
