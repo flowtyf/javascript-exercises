a70645a2e324a50771a7589d7f4b664f4f096eaf
flowtyf <taiao.kawiti@developersinstitute.co.nz>
2023-03-28 17:20:54 +1300

    Add soloution for excercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,22 @@
-const convertToCelsius = function() {
+const toOneDecimalPlace = (num) => {
+  num = Math.round(num * 10) / 10;
+  return num;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function (temp) {
+  let celsConversion = (temp - 32) * (5 / 9);
+  celsConversion = toOneDecimalPlace(celsConversion);
+  return celsConversion;
+};
+
+const convertToFahrenheit = function (temp) {
+  let fahrConversion = temp * (9 / 5) + 32;
+  fahrConversion = toOneDecimalPlace(fahrConversion);
+  return fahrConversion;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
